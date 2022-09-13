133c1fd1ce0683d7a83f9134e00fe40447c09311
Anthony Gorode <anthony.gorode@gmail.com>
2022-09-13 17:48:01 +1100

    delete private datas from firebase

--- firebase.js
+++ firebase.js
@@ -23,14 +23,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyA-l8pwnl_e6aE8NeCpu58JeqirGm9ih_o",
-    authDomain: "movix-app-dae11.firebaseapp.com",
-    databaseURL: "https://movix-app-dae11.firebaseio.com",
-    projectId: "movix-app-dae11",
-    storageBucket: "movix-app-dae11.appspot.com",
-    messagingSenderId: "927785726711",
-    appId: "1:927785726711:web:01b18790500b186c4b59f2",
-    measurementId: "G-ZSD74SQSX0"
+    
 };
 
 const app = initializeApp(firebaseConfig);
