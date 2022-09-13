26b819664c67c6ead9f27a1eaff66682147554a4
AnthonyGorode <47567403+AnthonyGorode@users.noreply.github.com>
2022-09-13 18:02:28 +1100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,7 @@
 // import { getFirestore } from 'firebase/firestore';
 
 // const app = getApps().length === 0 ? initializeApp({
-//     apiKey: "AIzaSyA-l8pwnl_e6aE8NeCpu58JeqirGm9ih_o",
-//     authDomain: "movix-app-dae11.firebaseapp.com",
-//     databaseURL: "https://movix-app-dae11.firebaseio.com",
-//     projectId: "movix-app-dae11",
-//     storageBucket: "movix-app-dae11.appspot.com",
-//     messagingSenderId: "927785726711",
-//     appId: "1:927785726711:web:01b18790500b186c4b59f2",
-//     measurementId: "G-ZSD74SQSX0"
+
 // }) : getApps()[0];
 
 // function firestore() {
